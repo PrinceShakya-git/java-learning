35d5a1f61f3cfccc09d9c7c5707232d6887594b2
PrinceShakya-git <davidlain2233@gmail.com>
2026-05-25 22:32:51 +0530

    Add files via upload

---
+++ Part-13/primerange.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class primerange {
+
+
+
+    public static boolean isPrime(int num){
+        if(num ==2){
+            return true;
+        }
+        for(int i = 2; i<Math.sqrt(num); i++){
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void primeinRange(int n){
+        for (int i =2 ; i<=n ; i++){
+            if(isPrime(i)){
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+
+
+
+
+    }
+    public static void main(String[]args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        primeinRange(20);
+
+    }
+}
+
+
+
