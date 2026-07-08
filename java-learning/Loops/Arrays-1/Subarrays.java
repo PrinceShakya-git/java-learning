aa7296dbdb76eaf0287817d993d6fd675ae5f203
PrinceShakya-git <davidlain2233@gmail.com>
2026-07-08 19:29:46 +0530

    Add files via upload

---
+++ java-learning/Loops/Arrays-1/Subarrays.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Subarrays {
+    public static void subarrays(int numbers[]){
+        int total = 0;
+        for(int i = 0 ; i<numbers.length; i++){
+            int start = i;
+            for(int j = i ; j<numbers.length; j++){
+                int end = j;
+                for(int k = start ; k<=end ; k++){
+                    System.out.print(numbers[k] + " ");
+                }
+                System.out.println();
+                total++;
+            }
+            System.out.println();
+        }
+        System.out.println("Total subarrays are : " + total);
+    }
+    public static void main(String[] args){
+        int numbers[] = {2,4,6,8,10};
+        subarrays(numbers);
+
+    }
+    
+}
